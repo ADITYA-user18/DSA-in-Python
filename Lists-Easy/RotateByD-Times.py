cde36e19cbc09ff021b529380c7ef408f507d041
Aditya G W <adityagw20@gmail.com>
2026-02-05 10:15:39 +0530

    Update project files

--- Lists-Easy/RotateByD-Times.py
+++ Lists-Easy/RotateByD-Times.py
@@ -17,3 +17,18 @@ a = DTimes()
 b = a.DTimesRotater([1,2,3,4,5,6,7,8,9],2)
 
 print(b)
+
+
+
+# class DTimes:
+#     def DTimesRotater(self,nums,d):
+#         n = len(nums)
+#         d = d%n
+
+#         return nums[d:]+nums[:d]
+    
+# a = DTimes()
+# b = a.DTimesRotater([1,2,3,4,5,6,7,8,9],3)
+# print(b)
+
+
