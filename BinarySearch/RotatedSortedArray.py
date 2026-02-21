047da275bf12e2231cbb6341ac2950dc9c99393b
Aditya G W <adityagw20@gmail.com>
2026-02-21 12:59:34 +0530

    Update project files

---
+++ BinarySearch/RotatedSortedArray.py
@@ -0,0 +1,36 @@
+class RotatedSortedArray:
+    def SortedArrayRotated(self,nums,X):
+        n = len(nums)
+        left =0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if nums[mid] == X:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= X < nums[mid]:
+                    right = mid-1
+
+                else:
+                    left = mid+1
+
+            else:
+
+                if nums[mid] < X <= nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+
+        return -1
+    
+
+a = RotatedSortedArray()
+b = a.SortedArrayRotated([4,5,6,7,0,1,2],0)
+print(b)
+    
+            
+
+
