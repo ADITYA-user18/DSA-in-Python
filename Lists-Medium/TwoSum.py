0dd33e04cd527f8e22c027901d3eee9b415fc3d2
Aditya G W <adityagw20@gmail.com>
2026-03-10 12:29:21 +0530

    Update project files

--- Lists-Medium/TwoSum.py
+++ Lists-Medium/TwoSum.py
@@ -1,21 +1,15 @@
-class TwoSum:
-    def TwoSumProblem(self,nums,X):
-        n = len(nums)-1
-        seen = set()
+class Solution(object):
+    def twoSum(self, numbers, target):
+         seen = {}
 
-
-        for i in range(n):
-            remain = X - nums[i]
+         for i in range(0,len(numbers)):
+            remain = target - numbers[i]
 
             if remain in seen:
-                return f"{nums[i]} and {remain}"
-            else:
-                seen.add(nums[i])
+                return [seen[remain]+1,i+1]
 
+            seen[numbers[i]] = i
 
-        return ''
-    
-a = TwoSum()
-b = a.TwoSumProblem([1,2,3,4,85,96,5,2,3,2,4,5,6,3,7,4,1,2,5],7)
-print((b))
+        
 
+        
