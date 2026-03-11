95ae3d15ab0d613137d1814f8a9f9a2e72e192df
Aditya G W <adityagw20@gmail.com>
2026-03-11 11:11:32 +0530

    Update project files

---
+++ Lists-Medium/WaterContainer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxArea(self, height):
+        n = len(height)
+        max_area = 0
+
+        for i in range(n-1):
+            for j in range(i+1, n):
+
+                width = j - i
+                h = min(height[i], height[j])
+                area = width * h
+
+             
+
+                max_area = max(max_area, area)
+
+               
+
+        return max_area
+
+
+height = [1,8,6,2,5,4,8,3,7]
+obj = Solution()
+print("Final Answer:", obj.maxArea(height))
