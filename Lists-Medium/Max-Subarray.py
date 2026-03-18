047e40dc9cd2974b620ed4f3e866af65faba2eb9
Aditya G W <adityagw20@gmail.com>
2026-03-18 16:36:14 +0530

    Fix syntax error in for loop in MaxiMumSubArray

--- Lists-Medium/Max-Subarray.py
+++ Lists-Medium/Max-Subarray.py
@@ -3,7 +3,7 @@ class MaxSubArray:
         n = len(nums)
         maxi = 0
 
-        for i in range(0,n):
+        for i in range(0,n)
             current_sum = 0
             for j in range(i,n):
                 current_sum+=nums[j]
