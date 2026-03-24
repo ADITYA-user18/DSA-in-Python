0ce5819aa83ab2c7483d9e11ca6cd99184b2d235
Aditya G W <adityagw20@gmail.com>
2026-03-24 09:43:37 +0530

    Implement maximum product calculation in Solution class

---
+++ Lists-Medium/3ELementProduct.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maximumProduct(self, nums):
+        nums.sort()
+
+
+        prod1 = nums[-1]*nums[-2]*nums[-3]
+
+        prod2 = nums[0]*nums[1]*nums[-1]
+
+
+        return max(prod1,prod2)
+       
+
