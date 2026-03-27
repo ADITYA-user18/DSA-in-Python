a9b603a9b14d1ee57b59792b0df601db2e9ac11b
Aditya G W <adityagw20@gmail.com>
2026-03-27 10:09:04 +0530

    Add maximumUniqueSubarray method to Solution class
    Implement maximumUniqueSubarray method to find the maximum sum of a unique subarray.

---
+++ Lists-Medium/RemoveSubsets.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+
+        left = 0
+        seen = set()
+        curr_sum = 0
+        maxi = 0
+
+
+        for right in range(len(nums)):
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                curr_sum-= nums[left]
+                left+=1
+            seen.add(nums[right])
+            curr_sum += nums[right]
+
+            maxi = max(maxi,curr_sum)
+
+        return maxi
+        
+        
+
+
+        
