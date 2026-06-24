d225be16c547c8712945d245e886e639d36f00c3
Aditya G W <adityagw20@gmail.com>
2026-06-24 17:31:23 +0530

    Create StrCompressor.js

---
+++ Strings/StrCompressor.js
@@ -0,0 +1,21 @@
+function Compressor(str){
+
+    let res = '';
+    let count=1;
+
+    for(let i=1;i<=str.length;i++){
+        if(str[i]==str[i-1]){
+            count+=1
+        }
+        else{
+            res += str[i-1]+count;
+            count=1
+        }
+    }
+
+    
+
+    return res
+}
+
+console.log(Compressor('aaabbc'))
