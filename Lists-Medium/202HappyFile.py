1875af87317822a43f3030b90c945d110d9175b9
Aditya G W <adityagw20@gmail.com>
2026-06-29 16:52:08 +0530

    Create 202HappyFile.py

---
+++ Lists-Medium/202HappyFile.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isHappy(self, n):
+
+        seen =  set()
+
+        def hi(n,seen):
+            if n==1:
+                return True
+
+            if n in seen:
+                return False
+
+            seen.add(n)
+
+            
+            string = str(n)
+            lister =  list(string)
+
+            for i in range(len(lister)):
+                lister[i]=int(lister[i])
+
+            total = 0
+
+            for i in range(len(lister)):
+                total+=lister[i]**2
+
+
+            return hi(total,seen)
+
+
+        return hi(n,seen)
+
+
+
+        
+
+        
