fbf0b5d2bf9dfd423a97b4d7b36929f49a0a0400
Aditya Wandakar <adityagw20@gmail.com>
2026-07-02 14:56:33 +0530

    Create 449lc.py

---
+++ Lists-Medium/449lc.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+
+        d = set()
+
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+
+                d.add(nums1[i])
+
+        return list(d)
+
+
+    
