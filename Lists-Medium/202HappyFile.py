b647a47e7af588858e22ffa14437a09ad1f329d6
Aditya Wandakar <adityagw20@gmail.com>
2026-07-23 18:33:25 +0530

    Update 202HappyFile.py

--- Lists-Medium/202HappyFile.py
+++ Lists-Medium/202HappyFile.py
@@ -5,7 +5,7 @@ class Solution(object):
 
         def hi(n,seen):
             if n==1:
-                return True
+                return True 
  
             if n in seen:
                 return False
